94f0e04964d4af7e21c7b78c22d8bc3bea5cf76e
T4v1n <142456068+T4v1n@users.noreply.github.com>
2024-03-01 10:08:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ("express")
+const app = express()
+
+app.listen(8081, function(){
+    console.log("Servidor Ativo")
+})
+
+app.get("/", function(req, res){
+    console.log("Página Principal")
+    res.send('Página Principal')
+})
+
+app.get("/contato", function(req, res){
+    console.log("Página contato")
+    res.send("Página contato")
+})
+
+app.get("/produtos/:item", function(req, res){
+    res.send("Página de Produtos: " + req.params.item)
+})
+
+app.get("/carros/:modelo/:ano/:cor", function(req,res){
+    res.send("Página de Carros: " + req.params.modelo + " Ano: " + req.params.ano + " Cor: " + req.params.cor)
+})
